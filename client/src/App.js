de7bb5a55a8e9aa772bbb9a1e62e8a8a5723e56d
Mario Krastev <mario.krastev@pytek.bg>
2022-11-04 17:01:01 +0200

    feat(apollo): added apollo / created queries [02]

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import Header from "./components/Header";
+import Clients from "./components/Clients";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const apoloClient = new ApolloClient({
+    uri: "http://localhost:5000/graphql",
+    cache: new InMemoryCache(),
+});
+
 function App() {
     return (
-        <div className="container">
-            <h1>Hello</h1>
-        </div>
+        <>
+            <ApolloProvider client={apoloClient}>
+                <Header />
+                <div className="container">
+                    <Clients />
+                </div>
+            </ApolloProvider>
+        </>
     );
 }
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const colors = require("colors");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
@@ -9,6 +10,8 @@ const app = express();
 
 //Connect DataBase
 connectDB();
+
+app.use(cors());
 
 app.use(
     "/graphql",
