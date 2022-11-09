d3eca96f612de3dbc5dc66f32bb0528ec4e277f3
Mario Krastev <mario.krastev@pytek.bg>
2022-11-09 17:57:45 +0200

    feat(delete): wired delete

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,23 @@ import Header from "./components/Header";
 import Clients from "./components/Clients";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+const cache = new InMemoryCache({
+    typePolicies: {
+        Query: {
+            fields: {
+                clients: {
+                    merge(existing, incoming) {
+                        return incoming;
+                    },
+                },
+            },
+        },
+    },
+});
+
 const apoloClient = new ApolloClient({
     uri: "http://localhost:5000/graphql",
-    cache: new InMemoryCache(),
+    cache,
 });
 
 function App() {
