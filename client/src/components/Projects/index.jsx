94e6f5ce057ba908ad069f231f114aa16321455f
Mario <mariookrastev@gmail.com>
2022-11-21 16:52:52 +0200

    :qw :wq

--- client/src/components/Projects/index.jsx
+++ client/src/components/Projects/index.jsx
@@ -5,25 +5,26 @@ import ProjectCard from "../ProjectCard";
 import Message from "../Message";
 
 export default function Projects() {
-    const { loading, error, data } = useQuery(GET_PROJECTS);
+  const { loading, error, data } = useQuery(GET_PROJECTS);
 
-    if (loading) return <Spinner />;
-    if (error) return <Message />;
-    return (
-        <>
-            {data.projects.length > 0 ? (
-                <div className="gap-1 row mb-5 border p-3">
-                    {data.projects.map((project) => (
-                        <ProjectCard key={project.id} project={project} />
-                    ))}
-                </div>
-            ) : (
-                <div className="d-flex mb-5 align-items-center justify-content-center border">
-                    <p className="m-0 text-uppercase fs-1 fw-bold p-5">
-                        No Available Projects
-                    </p>
-                </div>
-            )}
-        </>
-    );
+  if (loading) return <Spinner />;
+  if (error) return <Message />;
+  console.log(data);
+  return (
+    <>
+      {data.projects.length > 0 ? (
+        <div className="gap-1 row mb-5 border p-3">
+          {data.projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
+      ) : (
+        <div className="d-flex mb-5 align-items-center justify-content-center border">
+          <p className="m-0 text-uppercase fs-1 fw-bold p-5">
+            No Available Projects
+          </p>
+        </div>
+      )}
+    </>
+  );
 }
--- client/src/pages/Project/index.jsx
+++ client/src/pages/Project/index.jsx
@@ -9,42 +9,37 @@ import { borderColor, projectStatus } from "../../utils/projectStatus";
 import DeleteProject from "../../components/buttons/DeleteProject";
 
 export default function Project() {
-    const { id } = useParams();
-    const { loading, error, data } = useQuery(GET_PROJECT, {
-        variables: { id },
-    });
-
-    if (loading) return <Spinner />;
-    if (error) return <Message path={`/projects/${id}`} />;
-
-    return (
-        <>
-            {!loading && !error && (
-                <div
-                    className={`mx-auto w-75 card p-5 ${borderColor(
-                        data.project.status
-                    )}`}
-                >
-                    <Link
-                        to="/"
-                        className="btn btn-light d-inline ms-auto border border-secondary"
-                    >
-                        Go back
-                    </Link>
-                    <h1>{data.project.name}</h1>
-                    <p>{data.project.description}</p>
-                    <h5 className="mt-3">Project status</h5>
-                    <p
-                        className={`lead fw-bold ${projectStatus(
-                            data.project.status
-                        )}`}
-                    >
-                        {data.project.status}
-                    </p>
-                    <ClientInfo client={data.project.client} data={noClient} />
-                    <DeleteProject projectId={id} />
-                </div>
-            )}
-        </>
-    );
+  const { id } = useParams();
+  console.log(id);
+  const { loading, error, data } = useQuery(GET_PROJECT, {
+    variables: { id },
+  });
+  if (loading) return <Spinner />;
+  if (error) return <Message path={`/projects/${id}`} />;
+  return (
+    <>
+      {!loading && !error && (
+        <div
+          className={`mx-auto w-75 card p-5 ${borderColor(
+            data.project.status
+          )}`}
+        >
+          <Link
+            to="/"
+            className="btn btn-light d-inline ms-auto border border-secondary"
+          >
+            Go back
+          </Link>
+          <h1>{data.project.name}</h1>
+          <p>{data.project.description}</p>
+          <h5 className="mt-3">Project status</h5>
+          <p className={`lead fw-bold ${projectStatus(data.project.status)}`}>
+            {data.project.status}
+          </p>
+          <ClientInfo client={data.project.client} data={noClient} />
+          <DeleteProject projectId={id} />
+        </div>
+      )}
+    </>
+  );
 }
